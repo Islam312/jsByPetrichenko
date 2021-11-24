bf5a1bc93d743edb41c7ad23a38f462213c06e4b
Islam <alyshbekov96@gmail.com>
2021-11-24 10:45:55 +0600

    Javascript learning by Ivan Petrichenko is start

---
+++ Сезон Основы JavaScript/04 Интерполяция/index.js
@@ -0,0 +1,9 @@
+//* Интерполяция это улучшенный прием конкатинации
+
+const category = 'toys'
+
+console.log(`//https:someUrl.com/${category}`)
+
+const userName = 'Islam'
+
+console.log(`Hello, ${userName}`)
