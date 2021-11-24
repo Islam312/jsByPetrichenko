60fb6cb42051e900899a59a4d31e0b7ddb8e258b
Islam <alyshbekov96@gmail.com>
2021-11-24 22:09:53 +0600

    modified 08 operator if

--- Сезон Основы JavaScript/08 Условия/index.js
+++ Сезон Основы JavaScript/08 Условия/index.js
@@ -1,1 +1,30 @@
 //* Условия
+//* Каждое действие обусловлено условиями
+//* if(condition){
+//* code
+//*}
+
+//*Например:
+const obj = {
+  name: 'Islam',
+  isAdmin: true,
+};
+
+//*Обычный способ создания условий
+// if (obj.isAdmin === true) {
+//   console.log(`${obj.name} is admin`);
+// } else {
+//   console.log(`${obj.name} is not admin`);
+// }
+//* Тернарный способ создания условий(короткий)
+// (obj.isAdmin === true)?console.log(`${obj.name} is admin!`):console.log(`${obj.name} is not admin!`)
+
+//* Конструкция switch(){}
+switch (obj.isAdmin) {
+  case true:
+    console.log('You are admin!');
+    break;
+  case false:
+    console.log('You are not admin!');
+    break;
+}
