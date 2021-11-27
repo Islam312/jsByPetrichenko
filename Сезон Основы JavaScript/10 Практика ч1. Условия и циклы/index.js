1efd0053534339406ca51fef893ee26272ab21e1
Islam <alyshbekov96@gmail.com>
2021-11-27 15:06:55 +0600

    finished 10. Practice 1 . Condition and cycle.

--- Сезон Основы JavaScript/10 Практика ч1. Условия и циклы/index.js
+++ Сезон Основы JavaScript/10 Практика ч1. Условия и циклы/index.js
@@ -13,11 +13,39 @@ const personalMovieDB = {
   privat: false,
 };
 
-const lastMovie = prompt('Один из последних просмотренных фильмов?');
-const rateMovie = prompt('Оцените фильм?');
+for (let i = 0; i < 2; ) {
+  const lastMovie = prompt('Один из последних просмотренных фильмов?');
+  const rateMovie = +prompt('Оцените фильм?');
 
-personalMovieDB.movies[lastMovie] = rateMovie;
+  if (
+    lastMovie != null &&
+    lastMovie != '' &&
+    lastMovie.length < 50 &&
+    !isNaN(rateMovie) &&
+    rateMovie != '' &&
+    rateMovie > 0 &&
+    rateMovie != null
+  ) {
+    console.log('done');
+    personalMovieDB.movies[lastMovie] = rateMovie;
+    i++;
+    continue;
+  } else {
+    console.log('error');
+    alert('Ответ не действителен! Ответьте заново!');
+  }
+}
 
-console.log('personalMovieDB =>>', personalMovieDB)
+function countFilms(objCount) {
+  if (objCount.count < 0) {
+    alert('Произошла ошибка!');
+  } else if (objCount.count < 10) {
+    alert('Просмотрено довольно мало фильмов!');
+  } else if (objCount.count < 30) {
+    alert('Вы классический зритель!');
+  } else {
+    alert('Вы киноман!');
+  }
+}
 
- 
+countFilms(personalMovieDB);
